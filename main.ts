f813b2c5b0bdb17a0386167090405ccd839cea91
karsontsang23 <129140433+karsontsang23@users.noreply.github.com>
2023-06-21 11:16:40 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
+Sugar.onButtonEvent(DigitalPin.P0, function () {
+    if (Sugar.Angle(AnalogPin.P1) == 9) {
+        SugarBox.servo2kg(SugarBox.SPort.S1, 90)
+    }
+})
 SugarBox.waitready()
 basic.forever(function () {
     if (Sugar.Button(DigitalPin.P0)) {
         if (Sugar.Angle(AnalogPin.P1) == 9) {
-        	
+            SugarBox.servo2kg(SugarBox.SPort.S1, 90)
         }
     }
 })
