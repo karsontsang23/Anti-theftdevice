7fc58d9ba35a00e6e4481867258b350244565a28
karsontsang23 <129140433+karsontsang23@users.noreply.github.com>
2023-06-21 14:49:22 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
+let _ = 0
 Sugar.onButtonEvent(DigitalPin.P0, function () {
-    if (Sugar.Angle(AnalogPin.P1) == 9) {
+    if (_ == 9) {
         SugarBox.servo2kg(SugarBox.SPort.S1, 90)
         Sugar.ledOnoff(DigitalPin.P3, Sugar.LEDSta.On)
         Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.Off)
@@ -12,10 +13,10 @@ Sugar.onButtonEvent(DigitalPin.P2, function () {
     SugarBox.servo2kg(SugarBox.SPort.S1, 180)
     Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.On)
 })
-SugarBox.waitready()
 basic.forever(function () {
+    basic.showNumber(Sugar.Angle(AnalogPin.P1))
     if (Sugar.Button(DigitalPin.P0)) {
-        if (Sugar.Angle(AnalogPin.P1) == 9) {
+        if (_ == 9) {
             SugarBox.servo2kg(SugarBox.SPort.S1, 90)
             Sugar.ledOnoff(DigitalPin.P3, Sugar.LEDSta.On)
             Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.Off)
