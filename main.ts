3a0979f8bee4c51e62365bc9d09a540300b2a868
karsontsang23 <129140433+karsontsang23@users.noreply.github.com>
2023-06-22 13:24:48 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,37 @@
 Sugar.onButtonEvent(DigitalPin.P0, function () {
-    if (_3 == _2) {
+    if (AaBb == Password) {
         SugarBox.servo2kg(SugarBox.SPort.S1, 90)
         Sugar.ledOnoff(DigitalPin.P3, Sugar.LEDSta.On)
         Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.Off)
         music.play(music.stringPlayable("A B C5 B A B C5 B ", 200), music.PlaybackMode.InBackground)
         basic.pause(20000)
         SugarBox.servo2kg(SugarBox.SPort.S1, 180)
+        Sugar.ledOnoff(DigitalPin.P3, Sugar.LEDSta.Off)
         Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.On)
     } else {
         Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.On)
     }
 })
 Sugar.onButtonEvent(DigitalPin.P2, function () {
-    _3 += 1
+    AaBb += 1
 })
-let _3 = 0
-let _2 = 0
-_2 = 9
+let AaBb = 0
+let Password = 0
+Password = 9
+basic.forever(function () {
+    if (Sugar.Button(DigitalPin.P2)) {
+        AaBb += 1
+    }
+})
 basic.forever(function () {
     if (Sugar.Button(DigitalPin.P0)) {
-        if (_3 == _2) {
+        if (AaBb == Password) {
             SugarBox.servo2kg(SugarBox.SPort.S1, 90)
             Sugar.ledOnoff(DigitalPin.P3, Sugar.LEDSta.On)
             Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.Off)
             basic.pause(20000)
             SugarBox.servo2kg(SugarBox.SPort.S1, 180)
+            Sugar.ledOnoff(DigitalPin.P3, Sugar.LEDSta.Off)
             Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.On)
             music.play(music.stringPlayable("A B C5 B A B C5 B ", 200), music.PlaybackMode.InBackground)
         } else {
@@ -33,10 +40,5 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    basic.showNumber(_3)
-})
-basic.forever(function () {
-    if (Sugar.Button(DigitalPin.P2)) {
-        _3 += 1
-    }
+    basic.showNumber(AaBb)
 })
