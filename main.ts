650b7734c36891ecc60398935e917e991af0a338
karsontsang23 <129140433+karsontsang23@users.noreply.github.com>
2023-06-22 11:56:37 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,42 @@
-let _ = 0
 Sugar.onButtonEvent(DigitalPin.P0, function () {
-    if (_ == 9) {
+    if (_2 == _) {
         SugarBox.servo2kg(SugarBox.SPort.S1, 90)
         Sugar.ledOnoff(DigitalPin.P3, Sugar.LEDSta.On)
         Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.Off)
         music.play(music.stringPlayable("A B C5 B A B C5 B ", 200), music.PlaybackMode.InBackground)
+        basic.pause(20000)
+        SugarBox.servo2kg(SugarBox.SPort.S1, 180)
+        Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.On)
     } else {
         Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.On)
     }
 })
 Sugar.onButtonEvent(DigitalPin.P2, function () {
-    SugarBox.servo2kg(SugarBox.SPort.S1, 180)
-    Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.On)
+    _2 += 1
 })
+let _2 = 0
+let _ = 0
+_ = 9
 basic.forever(function () {
     if (Sugar.Button(DigitalPin.P0)) {
-        if (_ == 9) {
+        if (_2 == _) {
             SugarBox.servo2kg(SugarBox.SPort.S1, 90)
             Sugar.ledOnoff(DigitalPin.P3, Sugar.LEDSta.On)
             Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.Off)
+            basic.pause(20000)
+            SugarBox.servo2kg(SugarBox.SPort.S1, 180)
+            Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.On)
             music.play(music.stringPlayable("A B C5 B A B C5 B ", 200), music.PlaybackMode.InBackground)
         } else {
             Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.On)
-            music.play(music.stringPlayable("F F F F F F F F ", 500), music.PlaybackMode.InBackground)
         }
     }
 })
 basic.forever(function () {
+    basic.showNumber(_2)
+})
+basic.forever(function () {
     if (Sugar.Button(DigitalPin.P2)) {
-        SugarBox.servo2kg(SugarBox.SPort.S1, 180)
-        Sugar.ledOnoff(DigitalPin.P4, Sugar.LEDSta.On)
+        _2 += 1
     }
 })
